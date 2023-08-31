7816cea321016c06ef3ead85cc7299649781eb51
rick <richard19.perez77@yahoo.com>
2023-08-31 15:05:48 -0500

    deprecation suppression

--- app/src/main/java/com/compose/timerwithcolor/MainActivity.kt
+++ app/src/main/java/com/compose/timerwithcolor/MainActivity.kt
@@ -74,6 +74,7 @@ class MainActivity : ComponentActivity() {
 //        windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
 
         // TODO: update to WindowInsetsController
+        @Suppress("DEPRECATION")
         activity.window.decorView.systemUiVisibility =
             (View.SYSTEM_UI_FLAG_IMMERSIVE
                     or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
