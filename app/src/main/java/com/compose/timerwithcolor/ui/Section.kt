d5af6f5e0df6528ccf8881f998c2cb51ead3be8b
richa <richard19.perez77@yahoo.com>
2024-12-14 00:14:08 +0700

    opt imports

--- app/src/main/java/com/compose/timerwithcolor/ui/Section.kt
+++ app/src/main/java/com/compose/timerwithcolor/ui/Section.kt
@@ -5,14 +5,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import com.compose.timerwithcolor.ui.model.ColorMode
 import com.compose.timerwithcolor.ui.model.SectionState
 import com.compose.timerwithcolor.ui.theme.MikuLightGreen
