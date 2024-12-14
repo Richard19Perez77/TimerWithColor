a68e29d56597c35d190fe9faca11a8d1fd30de20
richa <richard19.perez77@yahoo.com>
2024-12-14 15:52:22 +0700

    tests added

--- app/src/androidTest/java/com/compose/timerwithcolor/SectionInstrumentedTest.kt
+++ app/src/androidTest/java/com/compose/timerwithcolor/SectionInstrumentedTest.kt
@@ -3,16 +3,12 @@ package com.compose.timerwithcolor
 import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class SectionInstrumentedTest {
 
@@ -21,25 +17,41 @@ class SectionInstrumentedTest {
 
     @Test
     fun testSection1BoxExists() {
-        var id = 1
-        composeTestRule.onNodeWithTag("Section{$id}Box").isDisplayed()
+        val id = 0
+        val interaction = composeTestRule.onNodeWithTag("Section{$id}Box")
+        composeTestRule.waitUntil { interaction.isDisplayed() }
     }
 
     @Test
     fun testTemp1TextExists() {
-        var id = 1
-        composeTestRule.onNodeWithTag("Temp{$id}Text").isDisplayed()
+        val id = 0
+        val interaction = composeTestRule.onNodeWithTag("Temp{$id}Text", true)
+        composeTestRule.waitUntil { interaction.isDisplayed() }
     }
 
     @Test
     fun testTimer1TextExists() {
-        var id = 1
-        composeTestRule.onNodeWithTag("Timer{$id}Text").isDisplayed()
+        val id = 0
+        val interaction = composeTestRule.onNodeWithTag("Timer{$id}Text", true)
+        composeTestRule.waitUntil { interaction.isDisplayed() }
     }
 
     @Test
     fun testDarkness1TextExists() {
-        var id = 1
-        composeTestRule.onNodeWithTag("Darkness{$id}Text").isDisplayed()
+        val id = 0
+        val interaction = composeTestRule.onNodeWithTag("Darkness{$id}Text", true)
+        composeTestRule.waitUntil { interaction.isDisplayed() }
+    }
+
+    @Test
+    fun testTemp1TextHSVExists() {
+        val interaction = composeTestRule.onNodeWithText("HSV")
+        composeTestRule.waitUntil { interaction.isDisplayed() }
+    }
+
+    @Test
+    fun testDarkness1TextHasDarknessExists() {
+        val interaction = composeTestRule.onNodeWithText("No Darkness")
+        composeTestRule.waitUntil { interaction.isDisplayed() }
     }
 }
